346a26cb443b3381ddd25ff05a240275d9a412d5
Miguel-Heijmans <58071327+Miguel-Heijmans@users.noreply.github.com>
2021-02-16 13:26:21 +0100

    Initial commit

---
+++ Assets/CoroutineMove.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CoroutineMove : MonoBehaviour
+{
+
+    public float speed;
+
+
+    private void Update() 
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            var target = GameObject.Find("Target");
+            StartCoroutine(MoveObject(target, 1.0f));
+
+            
+        }
+        
+        Debug.Log(GetComponent<Rigidbody>().velocity);
+    }
+
+    private IEnumerator MoveObject(GameObject target, float speed)
+    {
+        var direction = target.transform.position - transform.position;
+        GetComponent<Rigidbody>().velocity = direction * speed;
+        yield return null;
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == GameObject.Find("Target"))
+        {
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+    }
+    
+}
---
+++ Assets/Logger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Logger : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        //Start the coroutine we define below named ExampleCoroutine.
+        StartCoroutine(ExampleCoroutine());
+    }
+
+    IEnumerator ExampleCoroutine()
+    {
+
+
+        yield return new WaitForSeconds(0.5f);
+
+        
+        Debug.Log("ik start nu de coroutine : " + Time.time);
+
+        for (int i = 0; i < 10; i++)
+        {
+            
+            yield return new WaitForSeconds(0.5f);
+
+            
+            Debug.Log("coroutine update op : " + Time.time);
+        }
+
+
+        yield return new WaitForSeconds(0.5f);
+
+        Debug.Log("coroutine einde op : " + Time.time);
+    }
+
+
+}
